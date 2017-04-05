ea2fedd461590f698bac2444f01acb87f352b7df
Tlangelani Mboweni <mbowenit@gmail.com>
2017-04-05 16:58:55 +0200

    update classes

--- classes/intro.js
+++ classes/intro.js
@@ -1,17 +1,17 @@
+/**
+ * Lets create a User class the old way.
+ */
 function User(data) {
     this.firstname = data.firstname;
     this.lastname = data.lastname;
     this.age = data.age;
 }
-
 User.prototype.greet = function() {
     return `Hello ${this.firstname} ${this.lastname}, you are ${this.age} years old!`;
 };
-
 const user = new User({
     firstname: 'John',
     lastname: 'Doe',
     age: 22
 });
-
 console.log( user.greet() );
